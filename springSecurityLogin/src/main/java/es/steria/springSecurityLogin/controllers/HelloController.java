d663ecb767f0236e5b69cc99c86cf7e989dbecab
ckorbet <ckorbet@gmail.com>
2015-02-21 18:37:17 +0100

    master branch

---
+++ springSecurityLogin/src/main/java/es/steria/springSecurityLogin/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package es.steria.springSecurityLogin.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String printWelcome(final ModelMap model){
+		model.addAttribute("msg", "Spring Security hello World");
+		return "hello";
+	}
+}
